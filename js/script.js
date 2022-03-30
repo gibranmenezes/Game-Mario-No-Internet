76f0bf6c7d96d4f7280db57e9e87b274cb72345e
unknown <gibranquimica@gmail.com>
2022-03-30 09:29:54 -0300

    Created a version of a Dino Game

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const mario = document.querySelector('.mario');
+const background = document.querySelector('.background')
+
+let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event){
     if(event.keyCode === 32){
-       jump();
+        if(!isJumping){
+           jump();
+        }       
     }
 }
 
 function jump() {
-    let position = 0;
+    
+    isJumping = true;
     let upInterval = setInterval(() => {
         if(position >= 150){
             clearInterval(upInterval);
@@ -15,6 +22,7 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 0){
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                    position -= 20;
                    mario.style.bottom = position + 'px';     
@@ -27,4 +35,31 @@ function jump() {
     }, 20)
 }
 
+function createTortuga(){
+    const tortuga = document.createElement('div');
+    let tortugaPosition = 1000;
+    let randonTime = Math.random() * 6000;
+
+    tortuga.classList.add('tortuga');
+    tortuga.style.left = 1000 + 'px';
+    background.appendChild(tortuga);
+
+    let leftInterval = setInterval(() => {
+
+        if (tortugaPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(tortuga);
+        } else if (tortugaPosition > 0 && tortugaPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Game Over</h1>';
+        } else {    
+           tortugaPosition -= 10;
+           tortuga.style.left = tortugaPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createTortuga, randonTime);
+}
+
+createTortuga();
 document.addEventListener('keyup', handleKeyUp);
